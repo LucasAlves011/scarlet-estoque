1f64b6464f4d82ccfcd424aeaebbc220b353fd8b
Lucas Alves <lucas.nascimento.al16@gmail.com>
2023-04-14 15:55:50 -0300

    tudo ou nada

--- src/main/java/com/scarlet/backscarlet/service/VendaService.java
+++ src/main/java/com/scarlet/backscarlet/service/VendaService.java
@@ -28,9 +28,29 @@ public class VendaService {
                 var produto = produtoRepository.findById(solicitarItem.getProdutoId()).orElseThrow(
                         () -> new ObjectNotFoundException("Produto de id '" + solicitarItem.getProdutoId() + "' não encontrado."));
 
-                var tamanho = tamanhosEmMemoria.stream().filter(t -> t.verificarPorNome(solicitarItem.getTamanho().toString())).findFirst().get();
 
-                produto.retirarEstoque(tamanho, solicitarItem.getQuantidade());
+                switch (solicitarItem.getTamanho().toString()) {
+                    case "P" -> produto.retirarEstoque(tamanhosEmMemoria.get(0), solicitarItem.getQuantidade());
+                    case "M" -> produto.retirarEstoque(tamanhosEmMemoria.get(1), solicitarItem.getQuantidade());
+                    case "G" -> produto.retirarEstoque(tamanhosEmMemoria.get(2), solicitarItem.getQuantidade());
+                    case "GG" -> produto.retirarEstoque(tamanhosEmMemoria.get(3), solicitarItem.getQuantidade());
+                    case "T36" -> produto.retirarEstoque(tamanhosEmMemoria.get(4), solicitarItem.getQuantidade());
+                    case "T38" -> produto.retirarEstoque(tamanhosEmMemoria.get(5), solicitarItem.getQuantidade());
+                    case "T40" -> produto.retirarEstoque(tamanhosEmMemoria.get(6), solicitarItem.getQuantidade());
+                    case "T42" -> produto.retirarEstoque(tamanhosEmMemoria.get(7), solicitarItem.getQuantidade());
+                    case "T44" -> produto.retirarEstoque(tamanhosEmMemoria.get(8), solicitarItem.getQuantidade());
+                    case "T46" -> produto.retirarEstoque(tamanhosEmMemoria.get(9), solicitarItem.getQuantidade());
+                    case "T48" -> produto.retirarEstoque(tamanhosEmMemoria.get(10), solicitarItem.getQuantidade());
+                    case "T50" -> produto.retirarEstoque(tamanhosEmMemoria.get(11), solicitarItem.getQuantidade());
+                    case "T52" -> produto.retirarEstoque(tamanhosEmMemoria.get(12), solicitarItem.getQuantidade());
+                    case "T54" -> produto.retirarEstoque(tamanhosEmMemoria.get(13), solicitarItem.getQuantidade());
+                    case "T56" -> produto.retirarEstoque(tamanhosEmMemoria.get(14), solicitarItem.getQuantidade());
+                }
+//                var tamanho = tamanhosEmMemoria.stream().filter(t -> t.verificarPorNome(solicitarItem.getTamanho().toString())).findFirst();
+
+//                Tamanho x = tamanho.orElse(null);
+
+//                produto.retirarEstoque(x, solicitarItem.getQuantidade());
                 produtoRepository.save(produto);
             });
             return true;
