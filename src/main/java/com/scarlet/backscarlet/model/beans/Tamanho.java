0d2255b993ff53db232d17b1921172b28506da03
Lucas Alves <lucas.nascimento.al16@gmail.com>
2023-04-14 16:19:14 -0300

    HOTFIX 3.0

--- src/main/java/com/scarlet/backscarlet/model/beans/Tamanho.java
+++ src/main/java/com/scarlet/backscarlet/model/beans/Tamanho.java
@@ -4,11 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 
 @Getter
 @Setter
 @Entity(name = "tamanho")
+@ToString
 public class Tamanho {
 
     @Id
