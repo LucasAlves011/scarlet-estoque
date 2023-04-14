376ce5bcd800271675568ec548f4a183c791f43a
Lucas Alves <lucas.nascimento.al16@gmail.com>
2023-04-14 17:27:21 -0300

    seria a magia

--- src/main/java/com/scarlet/backscarlet/service/VendaService.java
+++ src/main/java/com/scarlet/backscarlet/service/VendaService.java
@@ -4,6 +4,7 @@ import com.scarlet.backscarlet.controller.exceptions.ObjectNotFoundException;
 import com.scarlet.backscarlet.controller.exceptions.UnidadesIndisponiveisException;
 import com.scarlet.backscarlet.model.beans.SolicitarItem;
 import com.scarlet.backscarlet.model.beans.Tamanho;
+import com.scarlet.backscarlet.model.enums.TamanhoEnum;
 import com.scarlet.backscarlet.model.repository.ProdutoRepository;
 import com.scarlet.backscarlet.model.repository.TamanhoRepository;
 import org.springframework.stereotype.Service;
@@ -32,6 +33,7 @@ public class VendaService {
 
                 switch (solicitarItem.getTamanho().toString()) {
                     case "P" -> produto.retirarEstoque(tamanhosEmMemoria.get(0), solicitarItem.getQuantidade());
+                    case "AVULSO" -> produto.retirarEstoque(tamanhosEmMemoria.get(1), solicitarItem.getQuantidade());
                     case "M" -> produto.retirarEstoque(tamanhosEmMemoria.get(2), solicitarItem.getQuantidade());
                     case "G" -> produto.retirarEstoque(tamanhosEmMemoria.get(3), solicitarItem.getQuantidade());
                     case "GG" -> produto.retirarEstoque(tamanhosEmMemoria.get(4), solicitarItem.getQuantidade());
@@ -43,7 +45,6 @@ public class VendaService {
                     case "T46" -> produto.retirarEstoque(tamanhosEmMemoria.get(10), solicitarItem.getQuantidade());
                     case "T48" -> produto.retirarEstoque(tamanhosEmMemoria.get(11), solicitarItem.getQuantidade());
                     case "T50" -> produto.retirarEstoque(tamanhosEmMemoria.get(12), solicitarItem.getQuantidade());
-                    case "AVULSO" -> produto.retirarEstoque(tamanhosEmMemoria.get(1), solicitarItem.getQuantidade());
                 }
                 produtoRepository.save(produto);
             });
@@ -57,7 +58,51 @@ public class VendaService {
                 () -> new ObjectNotFoundException("Produto de id '" + solicitarItem.getProdutoId() + "' não encontrado."));
 
 //        var tamanho = tamanhosEmMemoria.stream().filter( t -> t.verificarPorNome(solicitarItem.getTamanho().toString( ))).findFirst().get();
-        List.of("P", "M", "G", "GG", "T36", "T38", "T40", "T42", "T44", "T46", "T48", "T50", "AVULSO").stream().filter(t -> t.equals(solicitarItem.getTamanho().toString())).findFirst().get();
+//        List.of("P", "M", "G", "GG", "T36", "T38", "T40", "T42", "T44", "T46", "T48", "T50", "AVULSO").stream().filter(t -> t.equals(solicitarItem.getTamanho().toString())).findFirst().get();
+          switch (solicitarItem.getTamanho().toString()){
+              case "P" -> {
+                  return produto.verificarDisponibilidade(TamanhoEnum.P, solicitarItem.getQuantidade());
+              }
+              case "AVULSO" -> {
+                  return produto.verificarDisponibilidade(TamanhoEnum.AVULSO, solicitarItem.getQuantidade());
+              }
+              case "M" -> {
+                  return produto.verificarDisponibilidade(TamanhoEnum.M, solicitarItem.getQuantidade());
+              }
+              case "G" -> {
+                  return produto.verificarDisponibilidade(TamanhoEnum.G, solicitarItem.getQuantidade());
+              }
+              case "GG" -> {
+                  return produto.verificarDisponibilidade(TamanhoEnum.GG, solicitarItem.getQuantidade());
+              }
+              case "T36" -> {
+                  return produto.verificarDisponibilidade(TamanhoEnum.T36, solicitarItem.getQuantidade());
+              }
+              case "T38" -> {
+                  return produto.verificarDisponibilidade(TamanhoEnum.T38, solicitarItem.getQuantidade());
+              }
+              case "T40" -> {
+                  return produto.verificarDisponibilidade(TamanhoEnum.T40, solicitarItem.getQuantidade());
+              }
+              case "T42" -> {
+                  return produto.verificarDisponibilidade(TamanhoEnum.T42, solicitarItem.getQuantidade());
+              }
+              case "T44" -> {
+                  return produto.verificarDisponibilidade(TamanhoEnum.T44, solicitarItem.getQuantidade());
+              }
+              case "T46" -> {
+                  return produto.verificarDisponibilidade(TamanhoEnum.T46, solicitarItem.getQuantidade());
+              }
+              case "T48" -> {
+                  return produto.verificarDisponibilidade(TamanhoEnum.T48, solicitarItem.getQuantidade());
+              }
+              case "T50" -> {
+                  return produto.verificarDisponibilidade(TamanhoEnum.T50, solicitarItem.getQuantidade());
+              }
+
+          }
+
+
         return produto.verificarDisponibilidade(solicitarItem.getTamanho(), solicitarItem.getQuantidade());
     }
 
